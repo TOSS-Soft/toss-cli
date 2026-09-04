2e44b45ab1400e57b09e0ab192bc5d824f6de023
Cengiz Doyran <cengizdoyran@Cengiz-MacBook-Pro.local>
2026-09-05 01:45:53 +0300

    fix: remove embedded toss-core implementation

--- src/core/metadata.js
+++
@@ -1,6 +0,0 @@
-import {createRequire} from "node:module";
-
-const require=createRequire(import.meta.url);
-const packageMetadata=require("../../package.json");
-
-export const CORE_PACKAGE_VERSION=packageMetadata.version;
